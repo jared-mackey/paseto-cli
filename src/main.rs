68828901f8f6dcbace0e478dc8c0a3dfb3ec8074
Jared Mackey <jared@mackey.tech>
2021-01-26 10:00:53 -0700

    Additional error handling, exit codes

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,39 @@ fn main() {
     match env::args().nth(1) {
         Some(input) => {
             parse_string(input);
-            return;
         }
         None => match stdin.read_line(&mut input) {
             Ok(len) => {
                 if len == 0 {
-                    return;
+                    invalid_token();
                 } else {
                     parse_string(input);
-                    return;
                 }
             }
-            Err(error) => {
-                eprintln!("error: {}", error);
-                return;
+            Err(_error) => {
+                invalid_token();
             }
         },
     }
+    std::process::exit(0);
 }
 
 fn parse_string(token: String) -> () {
     let split: Vec<&str> = token.trim().split(".").collect();
     let token = String::from(split[2]);
-    let mut token =
-        base64::decode_config(&token, base64::URL_SAFE).expect("Could not decode base64");
-    token.truncate(token.len() - 64);
-    let token = String::from_utf8(token).expect("Could not convert back into string");
-    println!("{}", token);
+    if let Ok(mut token) = base64::decode_config(&token, base64::URL_SAFE) {
+        token.truncate(token.len() - 64);
+        if let Ok(token) = String::from_utf8(token) {
+            println!("{}", token);
+        } else {
+            invalid_token();
+        }
+    } else {
+        invalid_token();
+    }
+}
+
+fn invalid_token() {
+    eprintln!("Invalid token.");
+    std::process::exit(1);
 }
